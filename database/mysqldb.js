f72faed4ea8bf4577ee1a086a6145e9039186e97
psychblue <psychblue@naver.com>
2016-04-01 11:22:08 +0900

    Modifying SQL module

--- database/mysqldb.js
+++ database/mysqldb.js
@@ -7,16 +7,57 @@ var mysql = require("mysql");
 var logger = require("../logger/logger")(__filename);
 var mysqlDb = {};
 
+function mysqlLog(query, params){
+
+	var logString = query;
+
+	for(var paramIndex = 0; paramIndex < params.length; paramIndex++){
+		logString = logString.replace(/\?+/, params[paramIndex].toString());
+	}
+
+	logger.debug("SQL Query >>> [ " + logString + " ]");
+}
+
 //MySQL User DB Settings
 mysqlDb.init = function(options){
 
 	mysqlDb.connectionPool = mysql.createPool(options);
 	logger.info("MySQL User DB is connected...");
 };
+
+mysqlDb.doSQLQuery = function(options){
+
+	mysqlLog(options.query, options.params);
+
+	mysqlDb.connectionPool.getConnection(function(err, connection){
+		connection.query(query, params, function(err, rows, fields){
+			connection.release();
+		  if(err){
+		    logger.error("SQL Error >>> [ " + err.toString() + " ]");
+				options.onError(err);
+		  }
+			else{
+				if(options.onNoResult){
+				  if(rows.length === 0){
+						options.onNoResult();
+					}
+					else if(rows.length > 0){
+				    options.onSuccess(rows, fields);
+				  }
+				}
+				else{
+					options.onSuccess(rows, fields);
+				}
+			}
+		});
+	});
+};
 /*
 SELECT Function
 */
 mysqlDb.doSQLSelectQuery = function(query, params, callbackForSuccess, callbackForNoResult, callbackForError){
+
+	mysqlLog(query, params);
 
 	mysqlDb.connectionPool.getConnection(function(err, connection){
 		connection.query(query, params, function(err, rows, fields){
